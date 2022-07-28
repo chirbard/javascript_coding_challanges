5e23b6be00bb663e7743d5e4768fb438d6f8d321
chirbard <makuphon@gmail.com>
2022-07-28 20:13:01 +0300

    lihtne kuubik keerleb

---
+++ 002-merger-sponge-fractal/js/main.js
@@ -0,0 +1,48 @@
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x000000);
+
+// Add a cube to the scene
+const geometry = new THREE.BoxGeometry(1, 1, 1); // width, height, depth
+const material = new THREE.MeshLambertMaterial({ color: 0xfb8e00 });
+const mesh = new THREE.Mesh(geometry, material);
+mesh.position.set(0, 0, 0); // Optional, 0,0,0 is the default
+scene.add(mesh);
+
+// Set up lights
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
+scene.add(ambientLight);
+
+const dirLight = new THREE.DirectionalLight(0xffffff, 0.6);
+dirLight.position.set(10, 20, 0); // x, y, z
+scene.add(dirLight);
+
+// Perspective camera
+const aspect = window.innerWidth / window.innerHeight;
+const camera = new THREE.PerspectiveCamera(
+  45, // field of view in degrees
+  aspect, // aspect ratio
+  1, // near plane
+  100 // far plane
+);
+
+camera.position.set(4, 4, 4);
+camera.lookAt(0, 0, 0);
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.render(scene, camera);
+
+// Add it to HTML
+document.body.appendChild(renderer.domElement);
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  mesh.rotation.x += 0.01;
+  mesh.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+}
+
+animate();
